defea703bf826d0c4ecdd45ba3069f37a5ae91e9
mirzayogy <mirza.yogy@gmail.com>
2025-11-20 08:21:56 +0800

    templating #1

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Models\Matakuliah;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('template.default');
 });
 
 // Route::get('/matakuliah', function () {
