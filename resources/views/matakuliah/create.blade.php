01e57a2a74ebbeab2b2d77b022fc0c36842eb8c8
mirzayogy <mirza.yogy@gmail.com>
2025-11-27 08:49:10 +0800

    tambah data

--- resources/views/matakuliah/create.blade.php
+++ resources/views/matakuliah/create.blade.php
@@ -1,19 +1,42 @@
-Create Data
-<br>
-<form action="{{ route("matakuliah.store") }}" method="POST">
-    @csrf
-    @method('POST')
-    <label for="kode_mk">Kode</label>
-    <input type="text" name="kode_mk">
-    <br>
-    <label for="nama">Nama</label>
-    <input type="text" name="nama">
-    <br>
-    <label for="jumlah_sks">Jumlah SKS</label>
-    <input type="text" name="jumlah_sks">
-    <br>
-    <label for="prodi_id">Prodi ID</label>
-    <input type="text" name="prodi_id">
-    <br>
-    <button type="submit">Input</button>
-</form>
+@extends('template.default')
+@section('content')
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Tambah Matakuliah</h1>
+        <a href="/matakuliah" class="btn btn-primary btn-icon-split">
+            <span class="icon text-white-50">
+                <i class="fas fa-arrow-circle-left"></i>
+            </span>
+            <span class="text">Kembali</span>
+        </a>
+    </div>
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Matakuliah Baru</h6>
+        </div>
+        <div class="card-body">
+            <form action="{{ route('matakuliah.store') }}" method="POST">
+                @csrf
+                @method('POST')
+                <div class="mb-3">
+                    <label for="kode_mk" class="form-label">Kode Matakuliah</label>
+                    <input type="text" class="form-control" id="kode_mk" name="kode_mk" aria-describedby="emailHelp">
+                </div>
+                <label for="nama">Nama</label>
+                <input type="text" name="nama">
+                <br>
+                <label for="jumlah_sks">Jumlah SKS</label>
+                <input type="text" name="jumlah_sks">
+                <br>
+                <label for="prodi_id">Prodi ID</label>
+                <input type="text" name="prodi_id">
+                <br>
+                <button class="btn btn-success btn-icon-split" type="submit">
+                    <span class="icon text-white-50">
+                        <i class="fas fa-save"></i>
+                    </span>
+                    <span class="text">Simpan</span>
+                </button>
+            </form>
+        </div>
+    </div>
+@endsection
--- resources/views/matakuliah/index.blade.php
+++ resources/views/matakuliah/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Matakuliah</h1>
-        <a href="#" class="btn btn-success btn-icon-split">
+        <a href="{{ route('matakuliah.create') }}" class="btn btn-success btn-icon-split">
             <span class="icon text-white-50">
                 <i class="fas fa-plus"></i>
             </span>
